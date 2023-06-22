3ef3791d56a6c320e896603d2502890b52d0e741
ravipalnati <ravi.palnati34@gmail.com>
2023-06-22 11:21:44 +0530

    globaljson file (#6)
    creating global json files

---
+++ demos/demo6/demo4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https:// aka.ms/ new-console-template for more information
+Console.WriteLine(Environment.CurrentDirectory);
+Console.WriteLine(Environment.OSVersion.VersionString);
+Console.WriteLine(Environment.Version);
+Console.WriteLine(Environment.UserName);
+Console.WriteLine(Environment.MachineName);
+Console.WriteLine(Environment.ProcessorCount);
+Console.WriteLine(Environment.SystemDirectory);
+Console.WriteLine(Environment.UserDomainName);
+Console.WriteLine(Environment.UserInteractive);
+Console.WriteLine(Environment.WorkingSet);
+Console.WriteLine(Environment.Is64BitOperatingSystem);
+Console.WriteLine(Environment.Is64BitProcess);
