b03cb81871d6221000f43eca3629e21fff05aa99
ravipalnati <ravi.palnati34@gmail.com>
2023-08-03 11:06:48 +0530

    updated the folders with latest

---
+++ work/20230803/A1IntVariablesDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using HeaderFooter;
+
+Header _header = new();
+Footer _footer = new();
+
+_header.DisplayHeader('=', "A1 Integer Variables Demo");
+
+ShowIntegeralTypes();
+ShowIntegeralTypesV1();
+
+_footer.DisplayFooter('-');
+
+static void ShowIntegeralTypes()
+{
+    ForegroundColor = ConsoleColor.DarkCyan;
+
+    WriteLine($"Byte: {sizeof(byte)} byte(s) of memory. Its Min and Max Values are {byte.MinValue} and {byte.MaxValue}");
+    WriteLine($"SByte: {sizeof(sbyte)} byte(s) of memory. Its Min and Max Values are {sbyte.MinValue} and {sbyte.MaxValue}");
+
+    WriteLine($"Short: {sizeof(short)} byte(s) of memory. Its Min and Max Values are {short.MinValue} and {short.MaxValue}");
+    WriteLine($"UShort: {sizeof(ushort)} byte(s) of memory. Its Min and Max Values are {ushort.MinValue} and {ushort.MaxValue}");
+
+    ResetColor();
+}
+
+static void ShowIntegeralTypesV1()
+{
+    ForegroundColor = ConsoleColor.Magenta;
+
+    WriteLine($"Int: {sizeof(int)} byte(s) of memory. Its Min and Max Values are {int.MinValue} and {int.MaxValue}");
+    WriteLine($"UInt: {sizeof(uint)} byte(s) of memory. Its Min and Max Values are {uint.MinValue} and {uint.MaxValue}");
+
+    WriteLine($"Long: {sizeof(long)} byte(s) of memory. Its Min and Max Values are {long.MinValue} and {long.MaxValue}");
+    WriteLine($"ULong: {sizeof(ulong)} byte(s) of memory. Its Min and Max Values are {ulong.MinValue} and {ulong.MaxValue}");
+
+    ResetColor();
+}
---
+++ work/20230803/Readme.md
@@ -0,0 +1,23 @@
+# Session
+
+## Commands
+
+```powershell
+dotnet new sln -n 20230803
+
+dotnet new console -o A1IntVariablesDemo
+dotnet sln add .\A1IntVariablesDemo\A1IntVariablesDemo.csproj
+dotnet add reference ../../Common/HeaderFooter/HeaderFooter.csproj
+
+dotnet new console -o A2CharVariablesDemo
+dotnet sln add .\A2CharVariablesDemo\A2CharVariablesDemo.csproj
+dotnet add reference ../../Common/HeaderFooter/HeaderFooter.csproj
+
+dotnet new console -o A3BoolVariablesDemo
+dotnet sln add .\A3BoolVariablesDemo\A3BoolVariablesDemo.csproj
+dotnet add reference ../../Common/HeaderFooter/HeaderFooter.csproj
+
+dotnet new console -o A4FloatingVariablesDemo
+dotnet sln add .\A4FloatingVariablesDemo\A4FloatingVariablesDemo.csproj
+dotnet add reference ../../Common/HeaderFooter/HeaderFooter.csproj
+```
